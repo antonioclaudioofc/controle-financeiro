5de2095eacfae36178bbbf169f04b66c2262a856
Antonio Claudio <claudio1419teixeira2020@gmail.com>
2023-03-14 20:46:23 -0300

    Add logic commerce

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,52 @@ import './styles/main.css';
 import { Header } from './components/Header';
 import { Resume } from './components/Resume';
 import { Form } from './components/Form';
+import { useState, useEffect } from 'react';
+
+interface Transaction {
+    expense: boolean;
+    amount: number;
+}
 
 export function App() {
+    const [transactionsList, setTransactionsList] = useState<Transaction[]>([]);
+    const [income, setIncome] = useState<number>(0);
+    const [expense, setExpense] = useState<number>(0);
+    const [total, setTotal] = useState<number>(0);
+
+    useEffect(() => {
+        const data = localStorage.getItem('transactions');
+        if (data) {
+            setTransactionsList(JSON.parse(data));
+        }
+    }, []);
+
+    useEffect(() => {
+        const amountExpense = transactionsList
+            .filter((item) => item.expense)
+            .map((transaction) => transaction.amount);
+
+        const amountIncome = transactionsList
+            .filter((item) => !item.expense)
+            .map((transaction) => transaction.amount);
+
+        const expense = amountExpense.reduce((acc, cur) => acc + cur, 0);
+        const income = amountIncome.reduce((acc, cur) => acc + cur, 0);
+        const total = Math.abs(income - expense);
+
+        setIncome(income);
+        setExpense(expense);
+        setTotal(total);
+    }, [transactionsList]);
+
     return (
         <div className='bg-[#f2f2f2]'>
             <Header />
-            <Resume />
+            <Resume income={income} expense={expense} total={total} />
             <Form />
         </div>
     )
+
 }
+
+
--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -5,12 +5,18 @@ import {
     FaDollarSign
 } from 'react-icons/fa';
 
-export function Resume() {
+interface Summary {
+    income: number,
+    expense: number,
+    total: number
+}
+
+export function Resume(props: Summary) {
     return (
         <div className="max-w-6xl w-49/50 mx-auto my-0 -mt-14 flex justify-around gap-5 ">
-            <ResumeItem title="Entrada" icon={FaRegArrowAltCircleUp} value={12121} />
-            <ResumeItem title="Saídas" icon={FaRegArrowAltCircleDown} value={1212}/>
-            <ResumeItem title="Total" icon={FaDollarSign} value={100}/>
+            <ResumeItem title="Entrada" icon={FaRegArrowAltCircleUp} value={props.income} />
+            <ResumeItem title="Saídas" icon={FaRegArrowAltCircleDown} value={props.expense}/>
+            <ResumeItem title="Total" icon={FaDollarSign} value={props.total}/>
         </div>
     )
 }
